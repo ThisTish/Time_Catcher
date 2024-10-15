9ecae0d329e043eb116db41243c3673e0a6aeeff
Tish <tishthegoat@gmail.com>
2024-10-14 20:24:11 -0600

    color edit populate

--- components/CategoryForm.tsx
+++ components/CategoryForm.tsx
@@ -1,3 +1,5 @@
+// ! create goal closes diaglog, but create category isn't...
+
 // 'use client'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
@@ -42,33 +44,33 @@ interface CategoryFormProps {
 }
 
 
-const CategoryForm: React.FC<CategoryFormProps> =  ({status, categoryId}) => {
+const CategoryForm: React.FC<CategoryFormProps> = ({ status, categoryId }) => {
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			name:  '',
+			name: '',
 			color: undefined
 		}
 	})
 
 	useEffect(() => {
-		async function fetchCategory(){
+		async function fetchCategory() {
 			console.log(`status: ${status} categoryId: ${categoryId} categoryForm`)
-			if( status === 'edit' && categoryId){
-				const {data, error} = await getCategory(categoryId)
-				if(data){
-				console.dir(data)
+			if (status === 'edit' && categoryId) {
+				const { data, error } = await getCategory(categoryId)
+				if (data) {
+					console.dir(data)
 				}
-				if(error){
+				if (error) {
 					console.log(error)
 				}
 				form.reset({
 					name: data?.name || '',
 					color: data?.color || undefined
+					
 				})
-				// console.log(data?.color)
 			}
-			
+
 		}
 		fetchCategory()
 	}, [status, categoryId, form])
@@ -85,7 +87,7 @@ const CategoryForm: React.FC<CategoryFormProps> =  ({status, categoryId}) => {
 				description: `There was a problem with your request.${result.error}`
 			})
 		}
-	
+
 
 		if (result.data) {
 			toast({
@@ -94,106 +96,110 @@ const CategoryForm: React.FC<CategoryFormProps> =  ({status, categoryId}) => {
 			})
 			form.reset()
 		}
-}
-
-async function handleUpdateCategory(data: z.infer<typeof formSchema>) {
-	if(!categoryId) {
-		toast({
-			variant: 'destructive',
-			description: 'Error updating category'
-		})
-		return
 	}
 
-	const formData = new FormData()
-	formData.append('name', data.name)
-	formData.append('color', data.color)
-	console.log(formData)
-	const result = await editCategory(formData, categoryId)
-	if (result.error) {
-		toast({
-			variant: "destructive",
-			description: `There was a problem with your request.${result.error}`
-		})
+	async function handleUpdateCategory(data: z.infer<typeof formSchema>) {
+		if (!categoryId) {
+			toast({
+				variant: 'destructive',
+				description: 'Error updating category'
+			})
+			return
+		}
+
+		const formData = new FormData()
+		formData.append('name', data.name)
+		formData.append('color', data.color)
+
+		const result = await editCategory(formData, categoryId)
+		if (result.error) {
+			toast({
+				variant: "destructive",
+				description: `There was a problem with your request.${result.error}`
+			})
+		}
+
+
+		if (result.data) {
+			toast({
+				description: `Category ${result.data[0].name} added successfully!`,
+				duration: 4000
+			})
+			form.reset()
+		}
 	}
 
-
-	if (result.data) {
-		toast({
-			description: `Category ${result.data[0].name} added successfully!`,
-			duration: 4000
-		})
-		form.reset()
-	}
-}
-
-return (
-	<Form {...form}>
-		<form 
-		onSubmit={status === 'add' ? form.handleSubmit(handleCreateCategory) : form.handleSubmit(handleUpdateCategory)}
-		className="space-y-8">
-			{/* Category Name */}
-			<FormField
-				control={form.control}
-				name="name"
-				render={({ field }) => (
-					<FormItem>
-						<FormLabel>Name</FormLabel>
-						<FormControl>
-							<Input placeholder="Category Name" {...field} aria-description="Enter a category name"  required/>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
-			{/* Category Color */}
-			<FormField
-				control={form.control}
-				name="color"
-				render={({ field }) => (
-					<FormItem>
-						<FormLabel>Color</FormLabel>
-						<FormControl>
-							{/*
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={status === 'add' ? form.handleSubmit(handleCreateCategory) : form.handleSubmit(handleUpdateCategory)}
+				className="space-y-8">
+				{/* Category Name */}
+				<FormField
+					control={form.control}
+					name="name"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Name</FormLabel>
+							<FormControl>
+								<Input placeholder="Category Name" {...field} aria-description="Enter a category name" required />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{/* Category Color */}
+				<FormField
+					control={form.control}
+					name="color"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Color</FormLabel>
+							<FormControl>
+								{/*
 							Choose theme? other => choose color:
 							<input type="color" onValueChange={field.onchange value={field.value} 
 							<dataList rainbow=[blue, green, yellow, orange, red, purple, black, white, gray]>
 							
 							*/}
-							<Select onValueChange={field.onChange} required>
-								<SelectTrigger >
-									<SelectValue placeholder="Choose a Color" />
-								</SelectTrigger>
-								<SelectContent>
-									<SelectGroup>
-										{/* map over colors in theme with values... */}
-										<SelectLabel>Colors</SelectLabel>
-										<SelectItem value="BLUE">Blue</SelectItem>
-										<SelectItem value="GREEN">Green</SelectItem>
-										<SelectItem value="YELLOW">Yellow</SelectItem>
-										<SelectItem value="ORANGE">Orange</SelectItem>
-										<SelectItem value="RED">Red</SelectItem>
-										<SelectItem value="PURPLE">Purple</SelectItem>
-										<SelectItem value="PINK">Pink</SelectItem>
-										<SelectItem value="BROWN">Brown</SelectItem>
-										<SelectItem value="GREY">Grey</SelectItem>
-										<SelectItem value="BLACK">Black</SelectItem>
-										<SelectItem value="WHITE">White</SelectItem>
-									</SelectGroup>
-								</SelectContent>
-							</Select>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
-			{status === 'edit'?
-				<Button type="submit">SAVE</Button>
-				:
-				<Button  type="submit">ADD</Button>
-			}
-		</form>
-	</Form>
-)
+								<Select onValueChange={field.onChange} required>
+									<SelectTrigger >
+										{status === 'add' ?
+										<SelectValue placeholder="Choose a Color" /> :
+										<SelectValue placeholder={field.value}></SelectValue>
+									}
+									</SelectTrigger>
+									<SelectContent >
+										<SelectGroup>
+											{/* map over colors in theme with values... */}
+											<SelectLabel>Colors</SelectLabel>
+											
+											<SelectItem value="BLUE">Blue</SelectItem>
+											<SelectItem value="GREEN">Green</SelectItem>
+											<SelectItem value="YELLOW">Yellow</SelectItem>
+											<SelectItem value="ORANGE">Orange</SelectItem>
+											<SelectItem value="RED">Red</SelectItem>
+											<SelectItem value="PURPLE">Purple</SelectItem>
+											<SelectItem value="PINK">Pink</SelectItem>
+											<SelectItem value="BROWN">Brown</SelectItem>
+											<SelectItem value="GREY">Grey</SelectItem>
+											<SelectItem value="BLACK">Black</SelectItem>
+											<SelectItem value="WHITE">White</SelectItem>
+										</SelectGroup>
+									</SelectContent>
+								</Select>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{status === 'edit' ?
+					<Button type="submit">SAVE</Button>
+					:
+					<Button type="submit">ADD</Button>
+				}
+			</form>
+		</Form>
+	)
 }
 export default CategoryForm

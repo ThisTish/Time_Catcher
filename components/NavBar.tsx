b1661df909bbcd422e362204869eb866cd019762
Tish <tishthegoat@gmail.com>
2024-10-16 16:39:56 -0600

    time & date, and some format on landing

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -14,12 +14,14 @@ const NavBar = () => {
 				<Link href={"/"}><h1 className="text-3xl">TimeCatcher</h1></Link>
 				<nav className="flex gap-3 justify-center items-center text-sm">
 					<SignedOut>
+					<div className="bg-blue-400 text-white rounded-md p-1 text-lg border-2 border-blue-400 hover:bg-transparent hover:text-blue-400 hover:scale-110 transition-all ease-in">
 						<SignInButton />
+						</div>
 					</SignedOut>
 					<SignedIn>
 						{/* <SignOutButton /> */}
 						<CategoryDrawer status={'add'} />
-						<AddGoalDrawer categoryId=""/>
+						{/* <AddGoalDrawer status={'add'} /> */}
 						<UserButton appearance={{elements: {avatarBox: 'size-10'} }}/>
 					</SignedIn>
 					</nav>
